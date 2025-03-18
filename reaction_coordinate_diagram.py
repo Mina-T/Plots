d65f34a0d9ed43c8d727c794e3267cac0f610ef8
Mina-T <taleblou.mina@gmail.com>
2025-03-18 17:46:10 +0100

    first commit

---
+++ reaction_coordinate_diagram.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+
+class Reaction_diagram:
+    def __init__(self, data: dict):
+        self.labels = data['labels']
+        self.energies = data['energies']
+        self.steps = range(1, len(self.labels) +1)
+        self.barriers = data['barriers']
+
+    def draw_barrier(self, step, energy, label, barrier):
+        if barrier:
+            color = 'r'
+        else:
+            color = 'g'
+        plt.hlines(y= energy, xmin= step - 0.199, xmax= step + 0.199, linewidth=4, color=color)
+        plt.annotate(label, xy=(step, energy + 0.2))
+        
+    def connect_steps(self):
+        for i in range(len(self.steps)-1):
+            plt.plot([self.steps[i]+0.20, self.steps[i+1]-0.2], [self.energies[i], self.energies[i+1]],  ':b')
+
+    def plot_reaction_diagram(self):
+        fig = plt.figure(figsize=(10,5))
+        for step, energy, label, barrier in zip(self.steps, self.energies, self.labels, self.barriers):
+            self.draw_barrier(step, energy, label, barrier)
+        self.connect_steps()
+        plt.xticks([])
+        axs = plt.gca()
+        axs.spines['top'].set_visible(False)
+        axs.spines['right'].set_visible(False)
+        plt.yticks(fontsize = 13)
+        plt.xlabel('Reaction coordinates ------->', fontsize = 16, labelpad=15)
+        plt.ylabel('Energy (eV)', fontsize = 16, labelpad=15)
+        plt.show()
+
+
+
+
+# data = input('Enter the dictionary of {step: energy}: ')
+# def get_data(data):
+#     try:
+#         type(data) == dict
+#     except ValueError:
+#         print('Data type is not dictionary')
+
+data = {'labels': ['A', 'B', 'C', 'D'], 'energies': [1,5,3,4], 'barriers':[False, True, False, False]}
+inst = Reaction_diagram(data)
+inst.plot_reaction_diagram()
